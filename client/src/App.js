35de8954241cf44b5e4ba8c22d54f7f3b96a4ab2
Suman <sumanj631@gmai.com>
2021-12-09 16:49:40 +0530

    login button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Profile from "./components/Profile";
+import Authenticate from "./components/Authenticate";
 function App() {
   return (
     <>
@@ -16,6 +17,7 @@ function App() {
         <Route path="/home" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/profile" element={<Profile />}/>
+        <Route path="/login" element={<Authenticate />}/>
       </Routes>
     </Router>
     </>
