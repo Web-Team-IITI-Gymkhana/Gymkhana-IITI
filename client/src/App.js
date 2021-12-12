17870de3bb309af31907dbee2926724aaa1041a6
nehaj <nhjkhr.2021@gmail.com>
2021-12-12 21:51:40 +0530

    completed home page admin side, issue regarding images

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Profile from "./components/Profile";
+import './index.css';
 function App() {
   return (
     <>
-    <div className="App">
+    {/* <div className="App">
       <header className="App-header">
         <h1>Welcome to Gymkhana IITI</h1>
       </header>
-     </div>
+     </div> */}
 
     <Router>
       <Routes>
---
+++ client/src/components/Poster.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+function Poster() {
+  return (
+    <div>
+    
+      {/* <img
+            src="\client\src\components\photos\poster.jpg\photos\poster.jpg"
+            alt="Club Poster"
+            width="100%"
+            height="300px"
+          ></img> */}
+      <img
+        src="https://media.istockphoto.com/photos/any-questions-picture-id1296881945?b=1&k=20&m=1296881945&s=170667a&w=0&h=RZT0juI3SI1OLwjLQOamGnjADo9O6AAGRsjG3Kd4TsU="
+        alt="Club Poster"
+        width="100%"
+        height="350px"
+      ></img>
+      <Button variant="contained" id="edit-poster" color="primary">
+        Edit Poster
+      </Button>
+    </div>
+  );
+}
+
+export default Poster;
