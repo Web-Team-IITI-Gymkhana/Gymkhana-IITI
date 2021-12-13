d968cf81979020f86250834497cf37646d1a9708
nehaj <nhjkhr.2021@gmail.com>
2021-12-13 17:13:49 +0530

    image issue resolved

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import logo from "./photos/logo.jpg"
 function Logo() {
   return (
     <Grid className="logo" item xs={6}>
@@ -8,8 +9,9 @@ function Logo() {
         <div>
           <img
             id="logo"
+            src={logo}
             // src="https://image.shutterstock.com/image-vector/people-family-together-human-unity-600w-1296155950.jpg"
-            src="https://cdn.dribbble.com/users/2447187/screenshots/5962690/sand-dunes-final-2_4x.jpg"
+            // src="https://cdn.dribbble.com/users/2447187/screenshots/5962690/sand-dunes-final-2_4x.jpg"
             alt="logo"
             width="70%"
           ></img>
