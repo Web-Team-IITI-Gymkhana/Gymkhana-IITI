66558142f43a05df4486725ca6e98397b4edc74e
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-01-16 22:34:14 +0530

    Rem auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,56 @@
 import React from "react";
 import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 import Public from "./pages/public/Public";
 import Admin from "./pages/admin/Admin/Admin";
 
 import './index.css';
-import Authenticate from "./components/Auth/Authenticate";
+// import Authenticate from "./components/Auth/Authenticate";
 
-import configData from "./config.prod.json"
+// import configData from "./config.prod.json"
 
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 
 function App() {
 
-    const [user, setUser] = useState(null);
+    // const [user, setUser] = useState(null);
 
 
-    useEffect(() => {
-        const getUser =  () => {
-            console.log("Checking at URL ",configData.CHECK_URL)
+    // useEffect(() => {
+    //     const getUser =  () => {
+    //         console.log("Checking at URL ",configData.CHECK_URL)
 
-            fetch(configData.CHECK_URL, {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Credentials": true,
-                },
-            })
-            .then((response) => {
-                console.log("Google Response ",response)
-                if (response.status === 200) return response.json();
-                else{<Navigate to="/"/>}
-                throw new Error("authentication has been failed!");
-            })
-            .then((resObject) => {
-                console.log("resObject ",resObject)
-                setUser(resObject.user);
-            })
-            .catch((err) => {
-                console.log("Google Error", err);
-            });
-            console.log("After Fetch")
-        };
-        getUser();
-    }, []);
+    //         fetch(configData.CHECK_URL, {
+    //             method: "GET",
+    //             credentials: "include",
+    //             headers: {
+    //             Accept: "application/json",
+    //             "Content-Type": "application/json",
+    //             "Access-Control-Allow-Credentials": true,
+    //             },
+    //         })
+    //         .then((response) => {
+    //             console.log("Google Response ",response)
+    //             if (response.status === 200) return response.json();
+    //             else{<Navigate to="/"/>}
+    //             throw new Error("authentication has been failed!");
+    //         })
+    //         .then((resObject) => {
+    //             console.log("resObject ",resObject)
+    //             setUser(resObject.user);
+    //         })
+    //         .catch((err) => {
+    //             console.log("Google Error", err);
+    //         });
+    //         console.log("After Fetch")
+    //     };
+    //     getUser();
+    // }, []);
 
-    setUser("saasdads")
+    // console.log("GoogleUserAPI",user)
 
-    console.log("GoogleUserAPI",user)
-
-    console.log("GoogleUserCookie",Cookies.get('jwt'))
+    // console.log("GoogleUserCookie",Cookies.get('jwt'))
 
     return (
         <>
@@ -62,7 +60,8 @@ function App() {
                         <Routes>
                             <Route exact path="/" element = {<Navigate to="/public/home"/>}/>
                             <Route path="/public/*" element={<Public/>}/>
-                            <Route path="/admin/*" element={user ?<Admin/>:<Authenticate/>}/>
+                            {/* <Route path="/admin/*" element={user ?<Admin/>:<Authenticate/>}/> */}
+                            <Route path="/admin/*" element={<Admin/>}/>
                         </Routes>
                     </Router>
                 </div>
--- client/src/components/Auth/Authenticate.jsx
+++ client/src/components/Auth/Authenticate.jsx
@@ -1,11 +1,12 @@
 import React  from 'react'
 import Buttton from '@material-ui/core/Button'
-import configData from "./../../config.prod.json"
+// import configData from "./../../config.prod.json"
 
 export default function Authenticate() {
-        console.log("LOGIN URL IS", configData.LOGIN_URL)
+        // console.log("LOGIN URL IS", configData.LOGIN_URL)
         const googleLogin = ()=>{
-            window.open(configData.LOGIN_URL, "_self");
+            // window.open(configData.LOGIN_URL, "_self");
+            console.log("LOGIN")
         }
         return (
         <div>
