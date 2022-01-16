df821829902990aa9a2344894aa181291bf23148
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-01-16 21:34:18 +0530

    Remove jwt

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -1,31 +1,31 @@
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 const passport = require('passport')
-const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
+// const JwtStrategy = require('passport-jwt').Strategy
+// const ExtractJwt = require('passport-jwt').ExtractJwt
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config()
 
-const config = {secretOrKey:"mysecret"}
-var opts = {proxy : true,passport:true,}
-opts.jwtFromRequest = function(req) {
-    var token = null;
-    if (req && req.cookies){
-        token = req.cookies['jwt']
-    }
-    return token
-}
-opts.secretOrKey = config.secretOrKey
+// const config = {secretOrKey:"mysecret"}
+// var opts = {proxy : true,passport:true,}
+// opts.jwtFromRequest = function(req) {
+//     var token = null;
+//     if (req && req.cookies){
+//         token = req.cookies['jwt']
+//     }
+//     return token
+// }
+// opts.secretOrKey = config.secretOrKey
 
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    console.log("JWT BASED AUTH GETTING CALLED")
-    console.log(jwt_payload.data)
-    // if (CheckUser(jwt_payload.data)) {
-    //     return done(null, jwt_payload.data)
-    // } else {
-    //     return done(null, false)
-    // }
-    return done(null, jwt_payload.data)
-}))
+// passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+//     console.log("JWT BASED AUTH GETTING CALLED")
+//     console.log(jwt_payload.data)
+//     // if (CheckUser(jwt_payload.data)) {
+//     //     return done(null, jwt_payload.data)
+//     // } else {
+//     //     return done(null, false)
+//     // }
+//     return done(null, jwt_payload.data)
+// }))
 
 passport.use(new GoogleStrategy({
       clientID: process.env.CLIENT_ID,
@@ -40,8 +40,7 @@ passport.use(new GoogleStrategy({
       console.log("GOOGLE EMAIL ",email)
       if(email==='cse200001044@iiti.ac.in'){return done(null,profile)}
       return done(null, false)
-      // return done(null,profile)
-  }
+    }
 ))
 
 passport.serializeUser(function(user, done) {
