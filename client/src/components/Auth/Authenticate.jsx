e86a662bacac8f02d7813b42b6eb1d3bc1b9fb3c
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-01-17 13:03:10 +0530

    Rem google login

--- client/src/components/Auth/Authenticate.jsx
+++ client/src/components/Auth/Authenticate.jsx
@@ -1,16 +1,11 @@
 import React  from 'react'
-import Buttton from '@material-ui/core/Button'
-import configData from "./../../config.prod.json"
 
 export default function Authenticate() {
-        console.log("LOGIN URL IS", configData.LOGIN_URL)
-        const googleLogin = ()=>{
-            window.open(configData.LOGIN_URL, "_self");
-        }
+
         return (
         <div>
 
-            <Buttton onClick={googleLogin}>Login With Google</Buttton>
+            Authorization Page
 
         </div>
     )
