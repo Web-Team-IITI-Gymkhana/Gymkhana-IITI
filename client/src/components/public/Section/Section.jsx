d2529c45a129286ba089955a960e847fe38affb4
Suman Kumar Jaiswal <sumanj631@gmail.com>
2022-02-01 19:26:25 +0530

    Revert "env fixed"

--- client/src/components/public/Section/Section.jsx
+++ client/src/components/public/Section/Section.jsx
@@ -4,32 +4,32 @@ import Carousel from 'react-material-ui-carousel'
 import SectionChildCard from '../SectionChild/SectionChildCard';
 
 const Section = ({ section }) => {
-    section.sectionContent = section.sectionContent.filter(sectionChild => sectionChild.visible === true)
-    return (
-        <Box display={"flex"} flexDirection={"column"} alignItems={"center"} paddingY={5} >
+  section.sectionContent = section.sectionContent.filter(sectionChild => sectionChild.visible === true)
+  return (
+    <Box display={"flex"} flexDirection={"column"} alignItems={"center"} paddingY={5} >
 
-            {/*the heading, wrapped by two divs for skewing purposes*/}
-            <Box paddingX={5} paddingY={1} border={2} sx={{ transform: 'skew(-15deg)' }} marginBottom={4}>
-                <Box sx={{ transform: 'skew(15deg)' }}>
-                    <Typography variant={'h5'} fontWeight={'bold'} fontFamily={'arial'}>{section.sectionName}</Typography>
-                </Box>
-            </Box>
-
-
-
-            {/*the carousel*/}
-            <Paper sx={{ width: '90%', maxWidth: '1150px' }} elevation={3}>
-                <Carousel autoPlay={false}>
-                    {section.sectionContent.map(sectionChild =>
-                        <SectionChildCard
-                            key={sectionChild.sectionChildID}
-                            sectionName={section.sectionName}
-                            sectionChild={sectionChild} />)
-                    }
-                </Carousel>
-            </Paper>
+      {/*the heading, wrapped by two divs for skewing purposes*/}
+      <Box paddingX={5} paddingY={1} border={2} sx={{ transform: 'skew(-15deg)' }} marginBottom={4}>
+        <Box sx={{ transform: 'skew(15deg)' }}>
+          <Typography variant={'h5'} fontWeight={'bold'} fontFamily={'arial'}>{section.sectionName}</Typography>
         </Box>
-    )
+      </Box>
+
+      
+
+      {/*the carousel*/}
+      <Paper sx={{ width: '90%', maxWidth: '1150px' }} elevation={3}>
+        <Carousel autoPlay={false}>
+          {section.sectionContent.map(sectionChild => 
+            <SectionChildCard 
+              key={sectionChild.sectionChildID}
+              sectionName={section.sectionName} 
+              sectionChild={sectionChild} />)
+          }
+        </Carousel>
+      </Paper>
+    </Box>
+  )
 }
 
 export default Section
