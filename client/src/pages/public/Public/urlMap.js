50d6cca323a75a874ac05e7e4b7d07ff50e0071e
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-06-08 15:33:18 +0530

    Change client origin env

--- client/src/pages/public/Public/urlMap.js
+++ client/src/pages/public/Public/urlMap.js
@@ -1,6 +1,8 @@
 const urlMap = {
     "http://localhost:3000/public/home" : "Mihir",
-    "https://cynaptics-club.netlify.app/public/home" : "Cynaptics"
+    "https://gymkhana-iiti.netlify.app/public/home" : "Mihir",
+    "https://cynaptics-club.netlify.app/public/home" : "Cynaptics",
+    "https://p-club.netlify.app/public/home" : "PClub",
 }
 
 export default urlMap
--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ require('dotenv').config()
 
 const DB_URI = process.env.MONGO_URI
 const PORT = process.env.PORT || 5000;
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN
+const CLIENT_ORIGINS = process.env.CLIENT_ORIGINS
 
 const Users = require('./models/users')
 
-app.use(cors({ origin: CLIENT_ORIGIN, credentials: true }))
+app.use(cors({ origin: CLIENT_ORIGINS.split(' '), credentials: true }))
 app.use(express.json({ limit: '50mb' }))
 
 const cloudinary = require('cloudinary').v2
