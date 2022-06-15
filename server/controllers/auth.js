5658190280131789e84449489344625d064f4544
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-06-15 14:53:07 +0530

    migrate to google identity

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,29 +6,26 @@ require('dotenv').config();
 const client = new OAuth2Client("687468938838-qv69j02oai1engmjkd6el428ui4uquom.apps.googleusercontent.com")
 
 const googlelogin = (req,res) => {
-  const {tokenId} = req.body
-  client.verifyIdToken({idToken:tokenId,audience : "687468938838-qv69j02oai1engmjkd6el428ui4uquom.apps.googleusercontent.com"}).then(response=>{
-    const {email_verified,name,email} = response.payload;
-    if(email_verified)
-    {
-        Users.findOne({userEmailId:email}).exec((err,user)=>{
-          if(err){
-            return res.status(400).json({error:"Something went wrong"})
-          }
-          else{
-            if(user){
-              console.log("In auth controller",user)
-              const token = jwt.sign({name:user.userName,email:user.userEmailId},process.env.JWT_KEY,{expiresIn:'7d'});
-              console.log("Generating token ",token)
-              res.json({token,user:{userName : user.userName,userEmailId : user.userEmailId} })
-            }
-            else{
-              return res.status(400).json({error:"Something went wrong"})
-            }
-          }
-        })
+  const {email} = req.body
+
+  Users.findOne({userEmailId:email}).exec((err,user)=>{
+    if(err){
+      return res.status(400).json({error:"Something went wrong"})
+    }
+    else{
+      if(user){
+        console.log("In auth controller",user)
+        const token = jwt.sign({name:user.userName,email:user.userEmailId},process.env.JWT_KEY,{expiresIn:'7d'});
+        console.log("Generating token ",token)
+        res.json({token,user:{userName : user.userName,userEmailId : user.userEmailId} })
+      }
+      else{
+        return res.status(400).json({error:"Something went wrong"})
+      }
     }
   })
+
+
 }
 
 module.exports = { googlelogin}
