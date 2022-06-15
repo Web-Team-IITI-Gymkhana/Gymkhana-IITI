bb18f21a56417837e4c45656c52436f70ca67903
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-06-15 12:09:43 +0530

    Add pclub to map

--- client/src/pages/public/Public/urlMap.js
+++ client/src/pages/public/Public/urlMap.js
@@ -2,7 +2,7 @@ const urlMap = {
     "http://localhost:3000/public/home" : "Mihir",
     "https://gymkhana-iiti.netlify.app/public/home" : "Mihir",
     "https://cynaptics-club.netlify.app/public/home" : "Cynaptics",
-    "https://p-club.netlify.app/public/home" : "PClub",
+    "https://pclub-iiti.netlify.app/public/home" : "PClub",
 }
 
 export default urlMap
