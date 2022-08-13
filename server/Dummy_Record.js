a8f7f8ef07aad31cb2661ccc3321675cb37fe248
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-08-13 23:39:00 +0530

    add empty record

--- server/Dummy_Record.js
+++ server/Dummy_Record.js
@@ -1,175 +1,176 @@
-const cynaptics  = {
+const cynaptics =
+{
   "userName": "Mihir",
   "userEmailId": "cse200001044@iiti.ac.in",
   "publishedVersion": 1,
   "contentVersions": [
-  {
-  "userDetails": {
-  "name": "MIhir Karandikar",
-  "logo": "http://res.cloudinary.com/dofy83qux/image/upload/v1641382937/r0aldylmcfqobog2ypp1.jpg",
-  "socialMedia": {
-    "Instagram" : "www.instaCyn.com",
-    "Facebook" : "www.fbCyn.com",
-    "LinkedIn" : "www.linkedinCyn.com",
-    "Discord" : "www.discordCyn.com"
-  }
-  },
-  "homePagePoster": {
-  "src": "http://res.cloudinary.com/dofy83qux/image/upload/v1641287157/bwzsqfd65g6vrb6ylgi9.jpg",
-  "caption": "Caption Cyn"
-  },
-  "contactDetails": {
-  "email": "cynaptics@iiti.ac.in",
-  "phoneNumber": 123123
-  },
-  "contentVersion": 1,
-  "sectionSequence": [
-  "1",
-  "2"
-  ],
-  "Sections": [
-  {
-  "sectionName": "Events",
-  "sectionID": 1,
-  "sectionHeader": "Cynaptics Events",
-  "visible": true,
-  "sectionChildSequence": [
-  "1",
-  "2"
-  ],
-  "sectionContent": [
-  {
-  "sectionChildID": 1,
-  "sectionChildName": "Cynaptics Workshop 1",
-  "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641281338/k87iwwi0o6yluelw9yjq.jpg",
-  "sectionChildShortDesc": "Short Desc for Workshop 1",
-  "sectionChildDesc": "Detailed Desc for Workshop 1 ",
-  "sectionChildLinks": [
-  ""
-  ],
-  "visible": true
-  },
-  {
-  "sectionChildID": 2,
-  "sectionChildName": "Cynaptics Wokrshop 2",
-  "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641280763/jobpnbbgaltgjgt99lbb.png",
-  "sectionChildShortDesc": "Second Workshop",
-  "sectionChildDesc": "This is the latest workshop",
-  "sectionChildLinks": [
-  "kadk"
-  ],
-  "visible": true
-  }
-  ]
-  },
-  {
-  "sectionName": "Projects",
-  "sectionID": 2,
-  "sectionHeader": "Cynaptics Projects",
-  "visible": true,
-  "sectionChildSequence": [
-  "1"
-  ],
-  "sectionContent": [
-  {
-  "sectionChildID": 1,
-  "sectionChildName": "Project 1",
-  "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641287666/dh4zrnfylgwjhw37qvqn.png",
-  "sectionChildShortDesc": "Short Project Desc",
-  "sectionChildDesc": "Long Project Desc ",
-  "sectionChildLinks": [
-  "https://github.com/"
-  ],
-  "visible": true
-  }
-  ]
-  }
-  ],
-  "themeDetails": "goodtheme"
-  },
-  {
-  "userDetails": {
-  "name": "Abhinav Yadav",
-  "logo": "http://res.cloudinary.com/dofy83qux/image/upload/v1641382937/r0aldylmcfqobog2ypp1.jpg",
-  "socialMedia": {
-    "Instagram" : "www.instaCyn.com",
-    "Facebook" : "www.fbCyn.com",
-    "LinkedIn" : "www.linkedinCyn.com",
-    "Discord" : "www.discordCyn.com"
-  }
-  },
-  "homePagePoster": {
-  "src": "http://res.cloudinary.com/dofy83qux/image/upload/v1641287157/bwzsqfd65g6vrb6ylgi9.jpg",
-  "caption": "Caption Cyn"
-  },
-  "contactDetails": {
-  "email": "cynaptics@iiti.ac.in",
-  "phoneNumber": 123123
-  },
-  "contentVersion": 2,
-  "sectionSequence": [
-  "1",
-  "2"
-  ],
-  "Sections": [
-  {
-  "sectionName": "Events",
-  "sectionID": 1,
-  "sectionHeader": "Cynaptics Events",
-  "visible": true,
-  "sectionChildSequence": [
-  "1",
-  "2"
-  ],
-  "sectionContent": [
-  {
-  "sectionChildID": 1,
-  "sectionChildName": "Cynaptics Workshop 1",
-  "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641281338/k87iwwi0o6yluelw9yjq.jpg",
-  "sectionChildShortDesc": "Short Desc for Workshop 1",
-  "sectionChildDesc": "Detailed Desc for Workshop 1 ",
-  "sectionChildLinks": [
-  ""
-  ],
-  "visible": true
-  },
-  {
-  "sectionChildID": 2,
-  "sectionChildName": "Cynaptics Wokrshop 2",
-  "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641280763/jobpnbbgaltgjgt99lbb.png",
-  "sectionChildShortDesc": "Second Workshop",
-  "sectionChildDesc": "This is the latest workshop",
-  "sectionChildLinks": [
-  "[kadk](https://youtu.be/dQw4w9WgXcQ)"
-  ],
-  "visible": true
-  }
-  ]
-  },
-  {
-  "sectionName": "Projects",
-  "sectionID": 2,
-  "sectionHeader": "Cynaptics Projects",
-  "visible": true,
-  "sectionChildSequence": [
-  "1"
-  ],
-  "sectionContent": [
-  {
-  "sectionChildID": 1,
-  "sectionChildName": "Project 1",
-  "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641287666/dh4zrnfylgwjhw37qvqn.png",
-  "sectionChildShortDesc": "Short Project Desc",
-  "sectionChildDesc": "Long Project Desc ",
-  "sectionChildLinks": [
-  "https://github.com/"
-  ],
-  "visible": true
-  }
-  ]
-  }
-  ],
-  "themeDetails": "goodtheme"
-  }
+    {
+      "userDetails": {
+        "name": "MIhir Karandikar",
+        "logo": "http://res.cloudinary.com/dofy83qux/image/upload/v1641382937/r0aldylmcfqobog2ypp1.jpg",
+        "socialMedia": {
+          "Instagram": "www.instaCyn.com",
+          "Facebook": "www.fbCyn.com",
+          "LinkedIn": "www.linkedinCyn.com",
+          "Discord": "www.discordCyn.com"
+        }
+      },
+      "homePagePoster": {
+        "src": "http://res.cloudinary.com/dofy83qux/image/upload/v1641287157/bwzsqfd65g6vrb6ylgi9.jpg",
+        "caption": "Caption Cyn"
+      },
+      "contactDetails": {
+        "email": "cynaptics@iiti.ac.in",
+        "phoneNumber": 123123
+      },
+      "contentVersion": 1,
+      "sectionSequence": [
+        "1",
+        "2"
+      ],
+      "Sections": [
+        {
+          "sectionName": "Events",
+          "sectionID": 1,
+          "sectionHeader": "Cynaptics Events",
+          "visible": true,
+          "sectionChildSequence": [
+            "1",
+            "2"
+          ],
+          "sectionContent": [
+            {
+              "sectionChildID": 1,
+              "sectionChildName": "Cynaptics Workshop 1",
+              "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641281338/k87iwwi0o6yluelw9yjq.jpg",
+              "sectionChildShortDesc": "Short Desc for Workshop 1",
+              "sectionChildDesc": "Detailed Desc for Workshop 1 ",
+              "sectionChildLinks": [
+                ""
+              ],
+              "visible": true
+            },
+            {
+              "sectionChildID": 2,
+              "sectionChildName": "Cynaptics Wokrshop 2",
+              "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641280763/jobpnbbgaltgjgt99lbb.png",
+              "sectionChildShortDesc": "Second Workshop",
+              "sectionChildDesc": "This is the latest workshop",
+              "sectionChildLinks": [
+                "kadk"
+              ],
+              "visible": true
+            }
+          ]
+        },
+        {
+          "sectionName": "Projects",
+          "sectionID": 2,
+          "sectionHeader": "Cynaptics Projects",
+          "visible": true,
+          "sectionChildSequence": [
+            "1"
+          ],
+          "sectionContent": [
+            {
+              "sectionChildID": 1,
+              "sectionChildName": "Project 1",
+              "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641287666/dh4zrnfylgwjhw37qvqn.png",
+              "sectionChildShortDesc": "Short Project Desc",
+              "sectionChildDesc": "Long Project Desc ",
+              "sectionChildLinks": [
+                "https://github.com/"
+              ],
+              "visible": true
+            }
+          ]
+        }
+      ],
+      "themeDetails": "goodtheme"
+    },
+    {
+      "userDetails": {
+        "name": "Abhinav Yadav",
+        "logo": "http://res.cloudinary.com/dofy83qux/image/upload/v1641382937/r0aldylmcfqobog2ypp1.jpg",
+        "socialMedia": {
+          "Instagram": "www.instaCyn.com",
+          "Facebook": "www.fbCyn.com",
+          "LinkedIn": "www.linkedinCyn.com",
+          "Discord": "www.discordCyn.com"
+        }
+      },
+      "homePagePoster": {
+        "src": "http://res.cloudinary.com/dofy83qux/image/upload/v1641287157/bwzsqfd65g6vrb6ylgi9.jpg",
+        "caption": "Caption Cyn"
+      },
+      "contactDetails": {
+        "email": "cynaptics@iiti.ac.in",
+        "phoneNumber": 123123
+      },
+      "contentVersion": 2,
+      "sectionSequence": [
+        "1",
+        "2"
+      ],
+      "Sections": [
+        {
+          "sectionName": "Events",
+          "sectionID": 1,
+          "sectionHeader": "Cynaptics Events",
+          "visible": true,
+          "sectionChildSequence": [
+            "1",
+            "2"
+          ],
+          "sectionContent": [
+            {
+              "sectionChildID": 1,
+              "sectionChildName": "Cynaptics Workshop 1",
+              "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641281338/k87iwwi0o6yluelw9yjq.jpg",
+              "sectionChildShortDesc": "Short Desc for Workshop 1",
+              "sectionChildDesc": "Detailed Desc for Workshop 1 ",
+              "sectionChildLinks": [
+                ""
+              ],
+              "visible": true
+            },
+            {
+              "sectionChildID": 2,
+              "sectionChildName": "Cynaptics Wokrshop 2",
+              "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641280763/jobpnbbgaltgjgt99lbb.png",
+              "sectionChildShortDesc": "Second Workshop",
+              "sectionChildDesc": "This is the latest workshop",
+              "sectionChildLinks": [
+                "[kadk](https://youtu.be/dQw4w9WgXcQ)"
+              ],
+              "visible": true
+            }
+          ]
+        },
+        {
+          "sectionName": "Projects",
+          "sectionID": 2,
+          "sectionHeader": "Cynaptics Projects",
+          "visible": true,
+          "sectionChildSequence": [
+            "1"
+          ],
+          "sectionContent": [
+            {
+              "sectionChildID": 1,
+              "sectionChildName": "Project 1",
+              "sectionChildImage": "http://res.cloudinary.com/dofy83qux/image/upload/v1641287666/dh4zrnfylgwjhw37qvqn.png",
+              "sectionChildShortDesc": "Short Project Desc",
+              "sectionChildDesc": "Long Project Desc ",
+              "sectionChildLinks": [
+                "https://github.com/"
+              ],
+              "visible": true
+            }
+          ]
+        }
+      ],
+      "themeDetails": "goodtheme"
+    }
   ]
 }
---
+++ server/Empty_Record.js
@@ -0,0 +1,60 @@
+const cynaptics =
+{
+  "userName": "TestUser",
+  "userEmailId": "cse200001044@iiti.ac.in",
+  "publishedVersion": 1,
+  "contentVersions": [
+    {
+      "userDetails": {
+        "name": "Name",
+        "logo": "Logo",
+        "socialMedia": {
+          "Instagram": "",
+          "Facebook": "",
+          "LinkedIn": "",
+          "Discord": ""
+        }
+      },
+      "homePagePoster": {
+        "src": "",
+        "caption": ""
+      },
+      "contactDetails": {
+        "email": "",
+        "phoneNumber": 0
+      },
+      "contentVersion": 1,
+      "sectionSequence": [
+      ],
+      "Sections": [
+      ],
+      "themeDetails": ""
+    },
+    {
+      "userDetails": {
+        "name": "Name",
+        "logo": "Logo",
+        "socialMedia": {
+          "Instagram": "",
+          "Facebook": "",
+          "LinkedIn": "",
+          "Discord": ""
+        }
+      },
+      "homePagePoster": {
+        "src": "",
+        "caption": ""
+      },
+      "contactDetails": {
+        "email": "",
+        "phoneNumber": 0
+      },
+      "contentVersion": 2,
+      "sectionSequence": [
+      ],
+      "Sections": [
+      ],
+      "themeDetails": ""
+    }
+  ]
+}
